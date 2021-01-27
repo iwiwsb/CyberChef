54480b20a1c7ccfd2767ffa8ed60ecc2ae92271b
iwiwsb <30194859+iwiwsb@users.noreply.github.com>
2021-01-28 01:04:36 +0300

    Parse ID, QR and Opcode from header

--- src/core/operations/ParseDNSMessage.mjs
+++ src/core/operations/ParseDNSMessage.mjs
@@ -6,6 +6,8 @@
 
 import Operation from "../Operation.mjs";
 import OperationError from "../errors/OperationError.mjs";
+import { fromHex } from "../lib/Hex.mjs";
+import Utils from "../Utils.mjs";
 
 /**
  * Parse DNS Message operation
@@ -23,32 +25,76 @@ class ParseDNSMessage extends Operation {
         this.description = "Parse DNS message";
         this.infoURL = "https://wikipedia.org/wiki/Domain_Name_System";
         this.inputType = "string";
-        this.outputType = "JSON";
+        this.outputType = "string";
         this.args = [
-            /* Example arguments. See the project wiki for full details.
             {
-                name: "First arg",
-                type: "string",
-                value: "Don't Panic"
-            },
-            {
-                name: "Second arg",
-                type: "number",
-                value: 42
+                "name": "Input format",
+                "type": "option",
+                "value": ["Hex", "Raw"]
             }
-            */
         ];
     }
 
     /**
      * @param {string} input
      * @param {Object[]} args
-     * @returns {JSON}
+     * @returns {string}
      */
     run(input, args) {
-        // const [firstArg, secondArg] = args;
+        const format = args[0];
 
-        throw new OperationError("Test");
+        let inputBytes = [];
+        if (format === "Hex") {
+            inputBytes = fromHex(input);
+        } else if (format === "Raw") {
+            inputBytes = Utils.strToByteArray(input);
+        }
+
+        const dnsMessage = new Object();
+        dnsMessage.Header = new Object();
+
+        const inputBytesLength = inputBytes.length;
+
+        if (inputBytesLength < 12) {
+            throw new OperationError("Need 12 bytes for a DNS Message Header");
+        }
+
+        dnsMessage.Header.ID = inputBytes[0] * 0x16 + inputBytes[1];
+
+        dnsMessage.Header.QR = input[2] >> 7;
+        let qrType = "";
+        switch (dnsMessage.Header.QR) {
+            case 0:
+                qrType = "Message is a query";
+                break;
+            case 1:
+                qrType = "Message is a response";
+                break;
+            default:
+                throw new OperationError("Invalid message type");
+        }
+
+        dnsMessage.Header.opcode = (input[3] & 0b01111000) >> 3;
+        let opType = "";
+        switch (dnsMessage.Header.opcode) {
+            case 0:
+                opType = "QUERY: a standard query";
+                break;
+            case 1:
+                opType = "IQUERY: an inverse query";
+                break;
+            case 2:
+                opType = "STATUS: a server status request";
+                break;
+            default:
+                throw new OperationError("Invalid opcode. Values 3-15 reserved for future use");
+        }
+
+        const output = `Identifier: ${dnsMessage.Header.ID}
+Query type: ${dnsMessage.Header.QR.toString()} (${qrType})
+Opcode: ${dnsMessage.Header.opcode} (${opType})`;
+
+        return output;
     }
 
 }
