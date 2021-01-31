9e402218431f4a471025a5af3f1c81ad3ab8eb38
iwiwsb <30194859+iwiwsb@users.noreply.github.com>
2021-01-31 23:42:45 +0300

    Add class for Resourse Record; rewrite some things

--- src/core/operations/ParseDNSMessage.mjs
+++ src/core/operations/ParseDNSMessage.mjs
@@ -50,116 +50,142 @@ class ParseDNSMessage extends Operation {
             inputBytes = Utils.strToByteArray(input);
         }
 
-        const dnsMessage = new Object();
-        dnsMessage.Header = new Object();
+        /**
+         * Class for Resourse Record structure
+         */
+        class ResourceRecord {
+            /**
+             * Resourse Record structure constructor
+             */
+            constructor(name, type, dataClass, timeToLive, resourseDataLength, resourseData) {
+                this.type = type;
+                this.dataClass = dataClass;
+                this.timeToLive = timeToLive;
+                this.resourseDataLength = resourseDataLength;
+                this.resourseData = resourseData;
+            }
+        }
+
+        const DomainNameSystemMessage = new Object();
+        DomainNameSystemMessage.Header = new Object();
+        DomainNameSystemMessage.Question = new Object();
+        DomainNameSystemMessage.Answer = new ResourceRecord();
+
 
         if (inputBytes.length < 12) {
             throw new OperationError("Need 12 bytes for a DNS Message Header");
         }
 
-        dnsMessage.Header.ID = inputBytes[0] * 0x100 + inputBytes[1];
+        DomainNameSystemMessage.Header.id = inputBytes[0] * 0x100 + inputBytes[1];
 
         const QR = inputBytes[2] >> 7;
-        let qrType = "";
-        switch (QR) {
-            case 0:
-                qrType = "Message is a query";
-                break;
-            case 1:
-                qrType = "Message is a response";
-                break;
-            default:
-                qrType = "Invalid message type";
-        }
-        dnsMessage.Header.QR = `${qrType} (${QR})`;
+        const qrType = `Message is a ${(QR === 0) ? "query" : "response"}`;
+        DomainNameSystemMessage.Header.messageType = `${qrType} (${QR})`;
 
         const OPCODE = (inputBytes[2] >> 3) & 0b1111;
         const dnsOperations = [
-            {opName: "QUERY",  opDesc: "A standard query"},         // 0
-            {opName: "IQUERY", opDesc: "An inverse query"},         // 1
-            {opName: "STATUS", opDesc: "A server status request"},  // 2
-            {opName: "NOTIFY", opDesc: "Zone change notification"}, // 3
-            {opName: "DSO",    opDesc: "DNS Stateful Operations"}   // 4
+            {opName: "QUERY",      opDesc: "A standard query"},         // 0
+            {opName: "IQUERY",     opDesc: "An inverse query"},         // 1
+            {opName: "STATUS",     opDesc: "A server status request"},  // 2
+            {opName: "Unassigned", opDesc: ""},                         // 3
+            {opName: "NOTIFY",     opDesc: "Zone change notification"}, // 4
+            {opName: "UPDATE",     opDesc: "Dynamic update"},           // 5
+            {opName: "DSO",        opDesc: "DNS Stateful Operations"}   // 6
         ];
         let opText;
         if (OPCODE < 7) {
-            opText = `${dnsOperations[OPCODE].opName} - ${dnsOperations[OPCODE].opDesc}`;
+            opText = `${dnsOperations[OPCODE].opDesc} (${dnsOperations[OPCODE].opName})`;
         } else {
             opText = "Unknown operation";
         }
-        dnsMessage.Header.OPCODE = `${opText} (${OPCODE})`;
+        DomainNameSystemMessage.Header.opCode = `${opText} (${OPCODE})`;
 
         const TC = (inputBytes[2] >> 1) & 1;
-        const isTruncated = `Message is ${(TC === 0) ? "not " : ""}truncated`;
-        dnsMessage.Header.TC = `${isTruncated} (${TC})`;
+        const truncationDesc = `Message is ${(TC === 0) ? "not " : ""}truncated`;
+        DomainNameSystemMessage.Header.truncation = `${truncationDesc} (${TC})`;
 
         const RD = inputBytes[2] & 1;
-        const isRecursionDesired = `Do${RD === 1 ? "" : "n't do"} query recursevly`;
-        dnsMessage.Header.RD = `${isRecursionDesired} (${RD})`;
+        const recursionDesiredDesc = `Do${RD === 1 ? "" : "n't do"} query recursevly`;
+        DomainNameSystemMessage.Header.recursionDesired = `${recursionDesiredDesc} (${RD})`;
 
+        // True only for response messages
         if (QR === 1) {
             const AA = (inputBytes[2] >> 2) & 1;
-            const isAuthoritativeAnswer = `Server is ${(AA === 0) ? "not " : ""}an authority for domain`;
-            dnsMessage.Header.AA = `${isAuthoritativeAnswer} (${AA})`;
+            const authoritativeAnswerDesc = `Server is ${(AA === 0) ? "not " : ""}an authority for domain`;
+            DomainNameSystemMessage.Header.authoritativeAnswer = `${authoritativeAnswerDesc} (${AA})`;
 
             const RA = inputBytes[3] >> 7;
-            const isRecursionAvailable = `Server can${(RA === 1) ? "" : "'t"} do recursive queries`;
-            dnsMessage.Header.RA = `${isRecursionAvailable} (${RA})`;
+            const recursionAvailableDesc = `Server can${(RA === 1) ? "" : "'t"} do recursive queries`;
+            DomainNameSystemMessage.Header.recursionAvailable = `${recursionAvailableDesc} (${RA})`;
 
             const AD = (inputBytes[3] >> 5) & 1;
-            const isAuthenticated = `Answer/authority portion was ${(AD === 1) ? "" : "not "}authenticated by the server`;
-            dnsMessage.Header.AD = `${isAuthenticated} (${AD})`;
+            const authenticDataDesc = `Answer/authority portion was ${(AD === 1) ? "" : "not "}authenticated by the server`;
+            DomainNameSystemMessage.Header.authenticData = `${authenticDataDesc} (${AD})`;
 
             const CD = (inputBytes[3] >> 4) & 1;
-            const isCheckDisabled = `Non-authenticated data is ${(CD === 1) ? "" : "not "}acceptable`;
-            dnsMessage.Header.CD = `${isCheckDisabled} (${CD})`;
+            const checkDisabled = `Non-authenticated data is ${(CD === 1) ? "" : "not "}acceptable`;
+            DomainNameSystemMessage.Header.checkDisabled = `${checkDisabled} (${CD})`;
+
+            const RCODE = inputBytes[3] & 0b1111;
+
+            // Responses 0-5 described in RFC1035
+            // Responses 6-10 described in RFC2136
+            // Response 11 described in RFC8490
+            // Responses 16-18 described in RFC8945
+            // Responses 19-21 described in RFC2930
+            // Response 22 described in RFC8945
+            // Response 23 described in RFC7873
+            const dnsResponses = [
+                {responseName: "NoError",    errorDesc: "No Error"},                          // 0
+                {responseName: "FormErr",    errorDesc: "Format Error"},                      // 1
+                {responseName: "ServFail",   errorDesc: "Server Failure"},                    // 2
+                {responseName: "NXDomain",   errorDesc: "Non-Existent Domain"},               // 3
+                {responseName: "NotImp",     errorDesc: "Not Implemented"},                   // 4
+                {responseName: "Refused",    errorDesc: "Query Refused"},                     // 5
+                {responseName: "YXDomain",   errorDesc: "Name Exists when it should not"},    // 6
+                {responseName: "YXRRSet",    errorDesc: "RR Set Exists when it should not"},  // 7
+                {responseName: "NXRRSet",    errorDesc: "RR Set that should exist does not"}, // 8
+                {responseName: "NotAuth",    errorDesc: "Server Not Authoritative for zone"}, // 9
+                {responseName: "NotZone",    errorDesc: "Name not contained in zone"},        // 10
+                {responseName: "DSOTYPENI",  errorDesc: "DSO-TYPE Not Implemented"},          // 11
+                {responseName: "Unassigned", errorDesc: ""},                                  // 12
+                {responseName: "Unassigned", errorDesc: ""},                                  // 13
+                {responseName: "Unassigned", errorDesc: ""},                                  // 14
+                {responseName: "Unassigned", errorDesc: ""},                                  // 15
+                {responseName: "BADSIG",     errorDesc: "TSIG Signature Failure"},            // 16
+                {responseName: "BADKEY",     errorDesc: "Key not recognized"},                // 17
+                {responseName: "BADTIME",    errorDesc: "Signature out of time window"},      // 18
+                {responseName: "BADMODE",    errorDesc: "Bad TKEY Mode"},                     // 19
+                {responseName: "BADNAME",    errorDesc: "Duplicate key name"},                // 20
+                {responseName: "BADALG",     errorDesc: "Algorithm not supported"},           // 21
+                {responseName: "BADTRUNC",   errorDesc: "Bad Truncation"},                    // 22
+                {responseName: "BADCOOKIE",  errorDesc: "Bad/missing Server Cookie"},         // 23
+            ];
+
+            // Response described in RFC8495
+            const dnsUpdateNotAuthResponse = {responseName: "NotAuth", errorDesc: "Not Authorized"}; // 9
+
+            // Response described in RFC6891
+            const dnsExtensionBadVersResponse = {responseName: "BADVERS",  errorDesc: "Bad OPT Version"}; // 16
+
+            let errorText;
+            if (RCODE < 24) {
+                errorText = `${dnsResponses[RCODE].errorDesc} (${dnsResponses[RCODE].responseName})`;
+            } else if (RCODE >= 24 && RCODE < 3840 || RCODE >= 4096 && RCODE <= 65534) {
+                errorText = "No error assigned to this code";
+            } else if (RCODE >= 3841 && RCODE <= 4095) {
+                errorText = "Error code reserved for private use";
+            }
+            DomainNameSystemMessage.Header.responseCode = `${errorText} (${RCODE})`;
         }
 
-        dnsMessage.Header.Z = (inputBytes[3] >> 6) & 1;
+        DomainNameSystemMessage.Header.Z = (inputBytes[3] >> 6) & 1;
+        DomainNameSystemMessage.Header.QDCOUNT = inputBytes[4] * 0x100 + inputBytes[5];
+        DomainNameSystemMessage.Header.ANCOUNT = inputBytes[6] * 0x100 + inputBytes[7];
+        DomainNameSystemMessage.Header.NSCOUNT = inputBytes[8] * 0x100 + inputBytes[9];
+        DomainNameSystemMessage.Header.ARCOUNT = inputBytes[10] * 0x100 + inputBytes[11];
 
-        const errorCode = inputBytes[3] & 0b1111;
-        const dnsErrors = [
-            {errorName: "NoError",    errorDesc: "No Error"},                                           // 0
-            {errorName: "FormErr",    errorDesc: "Format Error"},                                       // 1
-            {errorName: "ServFail",   errorDesc: "Server Failure"},                                     // 2
-            {errorName: "NXDomain",   errorDesc: "Non-Existent Domain"},                                // 3
-            {errorName: "NotImp",     errorDesc: "Not Implemented"},                                    // 4
-            {errorName: "Refused",    errorDesc: "Query Refused"},                                      // 5
-            {errorName: "YXDomain",   errorDesc: "Name Exists when it should not"},                     // 6
-            {errorName: "YXRRSet",    errorDesc: "RR Set Exists when it should not"},                   // 7
-            {errorName: "NXRRSet",    errorDesc: "RR Set that should exist does not"},                  // 8
-            {errorName: "NotAuth",    errorDesc: "Server Not Authoritative for zone / Not Authorized"}, // 9
-            {errorName: "NotZone",    errorDesc: "Name not contained in zone"},                         // 10
-            {errorName: "DSOTYPENI",  errorDesc: "DSO-TYPE Not Implemented"},                           // 11
-            {errorName: "Unassigned", errorDesc: ""},                                                   // 12
-            {errorName: "Unassigned", errorDesc: ""},                                                   // 13
-            {errorName: "Unassigned", errorDesc: ""},                                                   // 14
-            {errorName: "Unassigned", errorDesc: ""},                                                   // 15
-            {errorName: "BADVERS",    errorDesc: "Bad OPT Version / TSIG Signature Failure"},           // 16
-            {errorName: "BADKEY",     errorDesc: "Key not recognized"},                                 // 17
-            {errorName: "BADTIME",    errorDesc: "Signature out of time window"},                       // 18
-            {errorName: "BADMODE",    errorDesc: "Bad TKEY Mode"},                                      // 19
-            {errorName: "BADNAME",    errorDesc: "Duplicate key name"},                                 // 20
-            {errorName: "BADALG",     errorDesc: "Algorithm not supported"},                            // 21
-            {errorName: "BADTRUNC",   errorDesc: "Bad Truncation"},                                     // 22
-            {errorName: "BADCOOKIE",  errorDesc: "Bad/missing Server Cookie"}                           // 23
-        ];
-        let errorText;
-        if (errorCode < 24) {
-            errorText = `${dnsErrors[errorCode].errorName} - ${dnsErrors[errorCode].errorDesc}`;
-        } else if (errorCode >= 24 && errorCode < 3840 || errorCode >= 4096 && errorCode <= 65534) {
-            errorText = "No error assigned to this code";
-        } else if (errorCode >= 3841 && errorCode <= 4095) {
-            errorText = "Error code reserved for private use";
-        }
-        dnsMessage.Header.RCODE = `${errorText} (${errorCode})`;
-
-        dnsMessage.Header.QDCOUNT = inputBytes[4] * 0x100 + inputBytes[5];
-        dnsMessage.Header.ANCOUNT = inputBytes[6] * 0x100 + inputBytes[7];
-        dnsMessage.Header.NSCOUNT = inputBytes[8] * 0x100 + inputBytes[9];
-        dnsMessage.Header.ARCOUNT = inputBytes[10] * 0x100 + inputBytes[11];
-
-        const output = dnsMessage;
+        const output = DomainNameSystemMessage;
         return output;
     }
 
